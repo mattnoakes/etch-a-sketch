7ea651bb37b3b7ee8b48a2ce22945bbc92dd5726
curly08 <mattynoakes08@gmail.com>
2021-04-28 11:21:08 -0400

    Add hover effect to change color of squares

--- scripts.js
+++ scripts.js
@@ -3,14 +3,20 @@ const container = document.querySelector('#container');
 
 for (let i=0; i<16; i++) {
     const row = document.createElement('div');
-
     row.setAttribute('class', 'row');
     container.appendChild(row);
     
     for (let e=0; e<16; e++) {
         const column = document.createElement('div');
-
         column.setAttribute('class', 'column');
         row.appendChild(column);
+
+        //hover effect
+        column.addEventListener('mouseover', () => {
+            column.setAttribute('class', 'hover');
+        });
+        // column.addEventListener('mouseout', () => {
+        //     column.setAttribute('class', 'column');
+        // });
     }
 }
