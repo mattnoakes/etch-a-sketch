0631907a4b0488dbccff12d31d1a36fca1706a5a
curly08 <mattynoakes08@gmail.com>
2021-04-28 08:48:43 -0400

    Edit height, witdth, margins, and padding to style.css and add squareDiv element to scripts.js for the grid

---
+++ scripts.js
@@ -0,0 +1,6 @@
+//create a 16x16 grid of square divs
+const container = document.querySelector('#container');
+const squareDiv = document.createElement('div');
+
+squareDiv.setAttribute('class', 'square');
+container.appendChild(squareDiv);
